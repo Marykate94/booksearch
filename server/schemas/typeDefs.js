a1c1eeb447f3665dd1c62beaa3d319c73a0c6921
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-09-10 15:59:18 -0400

    latest code for troubleshooting typeDef/resolver errors

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,46 +3,43 @@ const { gql } = require('apollo-server-express');
 
 // create typeDefs
 const typeDefs = gql`
-   type Book {
-        _id: ID
-        authors: [String]
-        description: String
-        bookId: String
-        image: String
-        forSale: String
-        link: String
-        title: String
-    }
-    type User {
-        _id: ID
-        username: String
-        email: String
-        bookCount: Int
-        savedBooks: [Book]        
-    }
-    type Query {
-        me: User
-    }
-    type Auth {
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
     token: ID!
-    user: User
-    }
-    input SavedBookInput {
-        authors: [String]
-        description: String
-        bookId: String
-        image: String
-        forSale: String
-        link: String
-        title: String
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: SavedBookInput): User
-        removeBook(bookId: String!): User
-}
-`;
+    users: User
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
+  input savedBook {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
+  }
+  `;
 
 // export the typeDefs
 module.exports = typeDefs;
